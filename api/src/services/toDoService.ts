1d229992c0af598c6329caf4a89823f398259c83
Mian Answer <manswer@farelogix.com>
2023-01-27 17:38:50 -0500

    Clean up code

--- api/src/services/toDoService.ts
+++ api/src/services/toDoService.ts
@@ -1,15 +1,15 @@
-export const addItem = async function() {
+export const addItem = async function () {
   console.log('insert to do list item')
 }
 
-export const getItem = async function() {
+export const getItem = async function () {
   console.log('get to do list item')
 }
 
-export const updateItem = async function() {
+export const updateItem = async function () {
   console.log('update to do list item')
 }
 
-export const deleteItem = async function() {
+export const deleteItem = async function () {
   console.log('delete to do list item')
 }
